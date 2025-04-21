2b15a0acde6cbba2960de3da8a6c1e03e0d55477
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-04-21 17:24:00 +0300

    chore: update imports from public api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+import { FORMULA_TEMPLATES } from './constants/templates';
+import { removePrefixSuffix } from './lib/removePrefixSuffix';
 export { default as Parser } from './main';
+export { FORMULA_TEMPLATES, removePrefixSuffix };
