ca2511aa7c0a8a801e71e1b04ada8056839af62d
laaane16 <nikon200402@gmail.com>
2025-05-14 13:58:25 +0300

    fix: literal default value should be with brackets

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -2,5 +2,5 @@ import { NUMBER_NODE_TYPE, LITERAL_NODE_TYPE } from './nodeTypes';
 
 export const defaultValues: Record<string, unknown> = {
   [NUMBER_NODE_TYPE]: 0,
-  [LITERAL_NODE_TYPE]: '',
+  [LITERAL_NODE_TYPE]: "''",
 };
