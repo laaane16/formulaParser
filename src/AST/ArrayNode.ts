3782540621a7e24ed613ca3d03b8b784dee22f6d
laaane16 <nikon200402@gmail.com>
2025-09-07 18:12:58 +0300

    chore: fix comment

--- src/AST/ArrayNode.ts
+++ src/AST/ArrayNode.ts
@@ -1,5 +1,3 @@
-
-
 import { ARRAY_NODE_TYPE } from '../constants/nodeTypes';
 import Token from '../Lexer/Token';
 import ExpressionNode from './ExpressionNode';
@@ -8,15 +6,15 @@ export default class NumberNode extends ExpressionNode {
   elements: ExpressionNode[] = [];
 
   constructor(leftPar: Token, elements: ExpressionNode[]) {
-    const {pos} = leftPar
+    const { pos } = leftPar;
 
     const startFuncExpression = pos;
     const endFuncExpression =
-      // if func hasn`t elements we should sum start pos, func name length and two brackets, else last elements length includes this length and one bracket
+      // if array hasn`t elements we should sum start pos and two brackets, else last elements length includes this length and one bracket
       elements[elements.length - 1]?.end + 2 || leftPar.pos + 1;
 
     super(ARRAY_NODE_TYPE, startFuncExpression, endFuncExpression);
-    
+
     this.elements = elements;
   }
 }
