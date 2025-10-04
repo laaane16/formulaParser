7f4eb63e4b56b1a8324c9f014d302be48bfbf5b8
Никита Никонов <nikon200402@gmail.com>
2025-10-04 20:50:12 +0300

    фикс recordId

--- src/Parser/mappers/functions/bpiumFunctions/js.ts
+++ src/Parser/mappers/functions/bpiumFunctions/js.ts
@@ -5,6 +5,6 @@ export const bpiumFunctionsToJsMap: Record<
   ValidBpiumFunctionsNamesWithSafe,
   IFormatterFunc
 > = {
-  RECORDID: (_, bpium) => `${bpium?.recordDbId ?? '#'}`,
+  RECORDID: (_, bpium) => `'${bpium?.recordDbId ?? '#'}'`,
   CATALOGID: (_, bpium) => `'${bpium?.catalogId ?? '#'}'`,
 };
