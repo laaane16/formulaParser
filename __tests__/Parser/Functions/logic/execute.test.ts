8e0efc1a6cf0a1509c3bdf697da4cd0273f5d91d
Никита Никонов <nikon200402@gmail.com>
2025-10-04 20:14:45 +0300

    update test

--- __tests__/Parser/Functions/logic/execute.test.ts
+++ __tests__/Parser/Functions/logic/execute.test.ts
@@ -3,7 +3,7 @@ import { Parser } from '../../../../src';
 describe('sql logic funcs', () => {
   test('isempty', () => {
     expect(new Parser('ISEMPTY(1)').toSqlWithVariables(true)).toBe(
-      "((1) IS NULL OR (1)::TEXT = '')",
+      "((1) IS NULL OR (1)::TEXT = '' OR (1)::TEXT = '{}')",
     );
   });
 });
